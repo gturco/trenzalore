ae16d32efad6857bd1f6325aec56645df3c948ec
Gina Turco <gturco88@gmail.com>
2013-07-31 12:17:18 -0700

    added generalpurpose code/ universal to all network types

---
+++ js/xgmml_to_json_generalpurpose.js
@@ -0,0 +1,115 @@
+#pip install beautifulsoup4
+### http://cytoscape.github.io/cytoscape.js/#notation/elements-json
+### https://github.com/bendtherules/GSOC_13/blob/master/nnf_and_sif_to_json_py/result.json
+from bs4 import BeautifulSoup
+import json
+import random
+from collections import OrderedDict
+
+
+
+def get_nstyle(xline):
+    style = xline.find_all("graphics")
+    faveColor = style[0].get("fill")
+    x = style[0].get("x")
+    y = style[0].get("y") 
+    try:
+	return float(x),float(y), faveColor
+    except TypeError:
+	return x,y,faveColor
+
+def get_estyle(xline):
+    style = xline.find_all("graphics")
+    faveColor = style[0].get("fill")
+    targetarrow = style[0].get("cy:targetarrow")
+    if targetarrow is None:
+        targetarrow = 7
+    return faveColor, targetarrow
+
+def get_size(xline):
+    atts = xline.find_all("att")
+    size_line = atts[1].get("value")
+    return size_line
+
+
+class NodeLine(object):
+    _slots_ = ("id","name","x","y","group","faveColor", "type")
+
+    def __init__(self, xline):
+        self.group = "nodes"
+        self.cid = xline.get("id")
+        self.name = xline.get("label")
+        self.x, self.y, self.faveColor = get_nstyle(xline)
+        self.nodeline = {"data" : {"id": self.cid, "name": self.name, "faveColor": self.faveColor : self.node_type},  "position": {"x": self.x, "y": self.y}}
+          
+
+class EdgeLine(object):
+    _slots_ = ("name","target","source","group","faveColor","classes","size")
+
+    def __init__(self, xline):
+        self.group = "edge"
+        self.name = xline.get("label")
+        self.source = xline.get("source")
+        self.target = xline.get("target")
+        self.faveColor, self.sign = get_estyle(xline)
+        self.size = get_size(xline)
+        self.edgeline = { "data": {"id": self.name, "source": self.source, "line-width": self.size,  "target": self.target, "fesign": self.sign, "naclsign": self.sign}}
+
+
+class Element(object):
+
+    def __init__(self,filename):
+        self.filename = filename
+        self.nodes = []
+        self.edges = []
+
+        fh = open(filename)
+        xgmml = fh.read()
+        soup = BeautifulSoup(xgmml)
+        for nline in soup.find_all("node"):
+            node = NodeLine(nline)
+            self.nodes.append(node)
+
+        for eline in soup.find_all("edge"):
+            edge = EdgeLine(eline)
+            self.edges.append(edge)
+
+
+    def get_dic(self):
+        n_dic = dict((n.cid, n.nodeline) for i,n in enumerate(self.nodes))
+        e_dic = dict((e.name, e.edgeline) for i,e in enumerate(self.edges))
+        return dict(n_dic.items() + e_dic.items())
+
+
+def stress(key,stress,stress_dic):
+        try:
+           cytoid = key['data']['id']
+           match_id = stress_dic[cytoid]
+           key['data'][stress] = 1
+           if '{0}sign'.format(stress) in list(match_id['data'].keys()):
+               key['data']['{0}sign'.format(stress)] = int(match_id['data']['{0}sign'.format(stress)])
+               return key
+           else: return key
+        except KeyError:
+            key['data'][stress] = 0
+            return key
+
+def main(xgmml_file, outfh):
+    outfile = open(outfh,"wb")
+    cyto = Element(xgmml_file)
+    nodelines = []
+    edgelines = []
+    cyto_d = cyto.get_dic()
+    for cytoid in cyto_d:
+        cyto_value = cyto_d[cytoid]
+        if "position" in cyto_value.keys():
+            nodelines.append(cyto_value)
+        else:
+            edgelines.append(cyto_value)
+
+    jsonformat = {"nodes": nodelines, "edges": edgelines}
+    jsonformated =  json.dumps(jsonformat,indent=4)
+    outfile.write(jsonformated)
+
+main("CytoscapeSession-2013_07_01-17_20//Sheet1.xgmml", "../generalpurpose_network.json")
+
